a97a24edbb62d1bdf4ae62845a9f04f1a577df71
曹发友(80016865) <fayou.cao@hxgroup.com>
2024-11-25 17:20:20 +0800

    从回调到Promise执行异步代码

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,55 @@
-import { renderCheckoutHeader } from './checkout/checkoutHeader.js'
+import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-renderCheckoutHeader();
-
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value2');
+    });
+  }),
+]).then((values) => {
+  console.log(values);
+  renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
+});
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
 })
+  .then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve('value2');
+      });
+    });
+  })
+  .then((value2) => {
+    console.log(value2);
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+*/
+
+/*
+loadProducts(()=>{
+  loadCart(()=>{
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+})
+*/
