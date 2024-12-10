70811ed22d9301e19ee39febeeaeedb69c8d17ff
曹发友(80016865) <fayou.cao@hxgroup.com>
2024-12-10 17:32:47 +0800

    增加跟踪百分数

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -23,6 +23,12 @@ async function loadPage() {
     }
   });
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(estimatedDeliveryTime);
+  const percentProgress =
+    ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
   document.querySelector('.js-order-tracking').innerHTML = `
       <a class="back-to-orders-link link-primary" href="orders.html">
         View all orders
@@ -43,19 +49,25 @@ async function loadPage() {
       <img class="product-image" src="${product.image}">
   
       <div class="progress-labels-container">
-        <div class="progress-label">
+        <div class="progress-label ${
+          percentProgress < 50 ? 'current-status' : ''
+        }">
           Preparing
         </div>
-        <div class="progress-label current-status">
+        <div class="progress-label ${
+          percentProgress >= 50 && percentProgress < 100 ? 'current-status' : ''
+        }">
           Shipped
         </div>
-        <div class="progress-label">
+        <div class="progress-label ${
+          percentProgress >= 100 ? 'current-status' : ''
+        }">
           Delivered
         </div>
       </div>
   
       <div class="progress-bar-container">
-        <div class="progress-bar"></div>
+        <div class="progress-bar" style="width: ${percentProgress}%;"></div>
       </div>
   `;
 }
